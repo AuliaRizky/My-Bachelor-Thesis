816c970920d0e2295b62050d2718180a5b1b8a0b
Arhking <31989033+AuliaRizky@users.noreply.github.com>
2019-02-17 19:35:09 +0700

    Update activation.py

--- activation.py
+++ activation.py
@@ -9,8 +9,7 @@ def display_activation(activations, col_size, row_size, act_index):
             ax[row][col].imshow(activation[0, :, :, activation_index], cmap='gray')
             activation_index += 1
             
-def activation_layer(images, model):
-    layer_outputs = [layer.output for layer in model.layers]
-    activation_model = Model(inputs=model.input, outputs=layer_outputs)
+def activation_layer(images, activation_model):
+    # May need to load weight
     activations = activation_model.predict(images)            
     display_activation(activations, 8, 8, 1)
